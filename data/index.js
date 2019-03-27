9835e577d7dd53becea4e1500aba263849eaa0d9
Andrew Schuessler <ajschuessler@gmail.com>
2019-03-27 17:48:16 -0400

    Generated random data for 100 properties. Created sqlite database properties.db with schema for data fields. Inserted data into database.

---
+++ data/index.js
@@ -0,0 +1,31 @@
+const sqlite3 = require('sqlite3').verbose();
+const data = require('./rawData');
+
+let db = new sqlite3.Database('/Users/andrewschuessler/Desktop/hackReactorProjects/frontEndCapstone/Neighborhood-Service/properties.db', (err) => {
+    if (err) {
+        console.log('error db', err);
+    } else {
+        console.log('connected to properties db!');
+    }
+});
+
+
+db.serialize(function() {
+   
+    // var stmt = db.prepare("INSERT INTO regoProperties VALUES (price, beds, baths, sqft, address, latitude, longitude)");
+    var stmt = db.prepare("INSERT INTO regoProperties VALUES (?, ?, ?, ?, ?, ?, ?)");
+    for (var i = 0; i < data.length; i++) {
+        stmt.run(data[i].price, data[i].beds, data[i].baths, data[i].sqft, data[i].address, data[i].latitude, data[i].longitude);
+    }
+    stmt.finalize();
+   
+});
+
+
+db.close((err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Closed the database connection.');
+});
+
