ec9ba5e42be2d35c5d69863d9bfffe8da88afbd1
Andrew Schuessler <ajschuessler@gmail.com>
2019-03-29 12:18:15 -0400

    Added 'MapProperty' react component to be used to place pins on 'Map'.

--- client/src/map.jsx
+++ client/src/map.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import config from '../../config.js';
+import MapProperty from './mapProperty.jsx';
 
 
 
@@ -23,16 +24,8 @@ class Map extends React.Component {
         return (
           // Important! Always set the container height explicitly
           <div style={{ height: '50vh', width: '50%' }}>
-            <GoogleMapReact
-              bootstrapURLKeys={{ key: config.GOOGLE_API_KEY }}
-              defaultCenter={this.props.center}
-              defaultZoom={this.props.zoom}
-            >
-              {/* <AnyReactComponent
-                lat={59.955413}
-                lng={30.337844}
-                text={'Kreyser Avrora'}
-              /> */}
+            <GoogleMapReact bootstrapURLKeys={{ key: config.GOOGLE_API_KEY }} defaultCenter={this.props.center} defaultZoom={this.props.zoom} >
+            <MapProperty lat={40.6782} lng={-73.9442}/>
             </GoogleMapReact>
           </div>
         );
---
+++ client/src/mapProperty.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+class MapProperty extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <img src={"https://media.istockphoto.com/photos/pig-picture-id120534386?k=6&m=120534386&s=612x612&w=0&h=nsy1ZpqPri6EfAaAOYgjLjpT5GjHGkgtEq0YTRiG6UU="} height="50" width="50"></img>
+            </div>
+        );
+    }
+}
+
+module.exports = MapProperty;
