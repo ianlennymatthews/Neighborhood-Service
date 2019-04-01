694a731007156ffbdfaa31d44aa447cd66bdf00b
Andrew Schuessler <ajschuessler@gmail.com>
2019-04-01 19:05:32 -0400

    nearbyHomes component dynamically changes pictures.

--- client/src/nearbyHomes.jsx
+++ client/src/nearbyHomes.jsx
@@ -6,19 +6,37 @@ class NearbyHomes extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            currentProperties: [this.props.properties[0], this.props.properties[1]]
+            currentFirstPicture: 0,
+            currentSecondPicture: 1,
         }
-        
+        this.slidePicsToRight = this.slidePicsToRight.bind(this);
+        this.slidePicsToLeft = this.slidePicsToLeft.bind(this);
+    }
+
+    slidePicsToRight() {
+        this.setState({
+            currentFirstPicture: this.state.currentFirstPicture + 2, 
+            currentSecondPicture: this.state.currentSecondPicture + 2
+        })
+    }
+
+    slidePicsToLeft() {
+        if (this.state.currentFirstPicture > 0) {
+            this.setState({
+                currentFirstPicture: this.state.currentFirstPicture - 2, 
+                currentSecondPicture: this.state.currentSecondPicture - 2
+            })
+        }
     }
 
     render() {
         return (
             <div>
                 <h5>NEARBY HOMES</h5>
-                <button id="nearbyHomesPicsScrollLeft">{'<'}</button>
-                <img className="nearbyHomesPics" src={this.state.currentProperties[0].imgUrl}></img>
-                <img className="nearbyHomesPics" src={this.state.currentProperties[1].imgUrl}></img>
-                <button id="nearbyHomesPicsScrollRight">{'>'}</button>
+                <button id="nearbyHomesPicsScrollLeft" onClick={this.slidePicsToLeft}>{'<'}</button>
+                <img className="nearbyHomesPics" src={this.props.properties[this.state.currentFirstPicture].imgUrl}></img>
+                <img className="nearbyHomesPics" src={this.props.properties[this.state.currentSecondPicture].imgUrl}></img>
+                <button id="nearbyHomesPicsScrollRight" onClick={this.slidePicsToRight}>{'>'}</button>
             </div>
         );
     }
