83dc0bc31ab6b34409750b5a70b3503ff098522e
Ian Matthews <ianlennymatthews@gmail.com>
2019-06-20 09:10:40 -0400

    Attempted implementation of mem-cache

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const cache = require('memory-cache');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
@@ -10,18 +11,36 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, '/../client/dist')));
 
+// configure cache middleware
+let memCache = new cache.Cache();
+let cacheMiddleware = duration => {
+  return (req, res, next) => {
+    let key = '__express__' + req.originalUrl || req.url;
+    let cacheContent = memCache.get(key);
+    if (cacheContent) {
+      res.send(cacheContent);
+      return;
+    } else {
+      res.sendResponse = res.send;
+      res.send = body => {
+        memCache.put(key, body, duration * 1000);
+        res.sendResponse(body);
+      };
+      next();
+    }
+  };
+};
+
 app.get('/items', (req, res) => {
-    Models.retrieveAll(req, res);
-})
+  Models.retrieveAll(req, res);
+});
 
 app.get('/items/:id', (req, res) => {
-    Models.retrieveOne(req, res);
-})
-
+  Models.retrieveOne(req, res);
+});
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
 });
-
 
 module.exports = app;
