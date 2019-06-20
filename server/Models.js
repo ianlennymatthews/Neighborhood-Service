1d828391922f3d7e820aa63040261474fab0104d
Ian Matthews <ianlennymatthews@gmail.com>
2019-06-20 09:19:33 -0400

    Fix CSV's being pushed to repo

--- server/Models.js
+++ server/Models.js
@@ -1,39 +1,37 @@
 const sqlite3 = require('sqlite3').verbose();
-
+const Neighborhoods = require('../MongoDB/index.js');
 
 var retrieveAll = (req, res) => {
-    let db = new sqlite3.Database(__dirname + '/../properties.db', (err) => {
-    
-        if (err) {
-            console.log('error db', err);
-        } else {
-            db.all("SELECT * FROM regoProperties;", [], (err, rows) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    res.send(rows);
-                }
-            });
-        }
-    });    
-}
+  // let db = new sqlite3.Database(__dirname + '/../properties.db', (err) => {
+  //     if (err) {
+  //         console.log('error db', err);
+  //     } else {
+  //         db.all("SELECT * FROM regoProperties;", [], (err, rows) => {
+  //             if (err) {
+  //                 console.log(err);
+  //             } else {
+  //                 res.send(rows);
+  //             }
+  //         });
+  //     }
+  // });
+};
 
 var retrieveOne = (req, res) => {
-    let db = new sqlite3.Database(__dirname + '/../properties.db', (err) => {
-        if (err) {
-            console.log('error db', err);
-        } else {
-            db.all("SELECT * FROM regoProperties WHERE uniqueId = " + req.params.id, [], (err, property) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    res.send(property);
-                }
-            })
-        }
-    })
-}
-
+  // let db = new sqlite3.Database(__dirname + '/../properties.db', (err) => {
+  //     if (err) {
+  //         console.log('error db', err);
+  //     } else {
+  //         db.all("SELECT * FROM regoProperties WHERE uniqueId = " + req.params.id, [], (err, property) => {
+  //             if (err) {
+  //                 console.log(err);
+  //             } else {
+  //                 res.send(property);
+  //             }
+  //         })
+  //     }
+  // })
+};
 
 module.exports.retrieveAll = retrieveAll;
 module.exports.retrieveOne = retrieveOne;
