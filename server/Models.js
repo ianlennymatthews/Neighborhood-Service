049395050de1b87518bcd6f81d817675159c8297
Ian Matthews <ianlennymatthews@gmail.com>
2019-06-20 09:26:59 -0400

    update routes

--- server/Models.js
+++ server/Models.js
@@ -1,7 +1,14 @@
-const sqlite3 = require('sqlite3').verbose();
+//const sqlite3 = require('sqlite3').verbose();
 const Neighborhoods = require('../MongoDB/index.js');
 
 var retrieveAll = (req, res) => {
+  Neighborhoods.find({}, 'id', (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(data);
+    }
+  });
   // let db = new sqlite3.Database(__dirname + '/../properties.db', (err) => {
   //     if (err) {
   //         console.log('error db', err);
@@ -18,6 +25,13 @@ var retrieveAll = (req, res) => {
 };
 
 var retrieveOne = (req, res) => {
+  Neighborhoods.findOne({ id: req.params.id }, (err, data) => {
+    if (err) {
+      console.log('error in db query: ', err);
+    } else {
+      res.send(data);
+    }
+  });
   // let db = new sqlite3.Database(__dirname + '/../properties.db', (err) => {
   //     if (err) {
   //         console.log('error db', err);
